ab814bd28ec0dbe283398b1e96ce993a1bad1769
Divyareddy33 <156607532+Divyareddy33@users.noreply.github.com>
2024-01-15 23:38:10 -0600

    Add files via upload

--- question3.py
+++ question3.py
@@ -1,7 +1,19 @@
 def main():
-    str = input("Input a sentence: ")
-    newStr = str.replace('python', 'pythons')
-    print(newStr)
+    while True:
+        score = int(input('Enter the input score: '))
+
+        if 90 <= score <= 100:
+            print("A")
+        elif 80 <= score <= 89:
+            print("B")
+        elif 70 <= score <= 79:
+            print("C")
+        elif 60 <= score <= 69:
+            print("D")
+        elif 0 <= score <= 59:
+            print("F")
+        else:
+            print("Input Valid Score!!")
 
 
 if _name_ == '_main_':
